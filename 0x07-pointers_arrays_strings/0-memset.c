6346af7b7cbe68a036a23e3814e829345ecab532
Drezeen <africanblackgodzilla@gmail.com>
2022-09-26 14:22:11 -0700

    My personal work

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+# include "main.h"
+/**
+ * _memset - fill a memory block with constant byte
+ * @s: address to memory block
+ * @b: chart be used
+ * @n: number for byte to be used
+ *
+ * return pointer to the memory block
+ */
+char *_ memset(char *s, char b, unsigned int n)
+{
+	while
+	{
+		s(n - 1) = b;
+		n++;
+	} 
+	return (s);
+}
+
+
