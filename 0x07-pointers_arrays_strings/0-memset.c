2b0d1a605316368d733d115f171dd56644274429
Drezeen <africanblackgodzilla@gmail.com>
2022-09-27 00:25:10 -0700

    My c programming language work

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,17 @@
 # include "main.h"
+
 /**
- * _memset - fill a memory block with constant byte
+ * _memset - fill a memory block with a constant byte
  * @s: address to memory block
  * @b: chart be used
- * @n: number for byte to be used
+ * @n: number of byte to be used
  *
  * return pointer to the memory block
  */
+
 char *_ memset(char *s, char b, unsigned int n)
 {
-	while
+	while(n)
 	{
 		s(n - 1) = b;
 		n++;
