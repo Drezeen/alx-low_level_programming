700c8c140d68f13d380ea9460058d3d35fff6a16
Drezeen <africanblackgodzilla@gmail.com>
2022-09-27 06:49:03 -0700

    My personal work

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#  include "main.h"
+
+/**
+ * set_string - set the value of pointer to char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
+ */
+void set_string(char **s, chard *to)
+{
+	*s = to;
+}
+
