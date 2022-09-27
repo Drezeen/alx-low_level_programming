6bf55298442a44dd7af3bb4189a80b517f359b40
Drezeen <africanblackgodzilla@gmail.com>
2022-09-27 00:53:45 -0700

    My c programming work

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include " main.h"
+
+/**
+ * _memcpy - copies a memory area
+ * @dest: memory area to be copied to
+ * @src : memory area to be copied from
+ * @n: number of byte to be copied
+ *
+ * Return: pointer to the copied memory block
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int 1 = 0;
+
+	for (; I < n; 1++)
+	{
+		dest[I] = src[I]
+	}
+
+	return (dest);
+}
+
