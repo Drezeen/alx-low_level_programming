d46737f47eddbd749e0ab24bb88c68933f1a5b39
Drezeen <africanblackgodzilla@gmail.com>
2022-09-30 11:35:14 -0700

    My personal work

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main -> this the function to print it's name
+ * @argc: agrc parameter
+ * argv: an array of a command listed
+ * Return: 0 for success 
+ */
+int main(argc _attribute_((unused)), char *agrv[])
+{
+	printf("%s\n", *agr);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MIAN_H
+#define MAIN_H
+
+_putchar(char c);
+check_num(char *str);
+#endif
+
