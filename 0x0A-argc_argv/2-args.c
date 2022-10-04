2bb6e0d586aa57139160c16d24ed28ccfdc9d959
Drezeen <africanblackgodzilla@gmail.com>
2022-10-04 04:10:46 -0700

    My personal work

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main -> this the function to print it's name
- * @argc: agrc parameter
- * argv: an array of a command listed
- * Return: 0 for success 
+ * main - prints all arguments it recieves
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
  */
-int main(int argc, char *agrv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	 
+
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", agrv[i]);
-	
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
