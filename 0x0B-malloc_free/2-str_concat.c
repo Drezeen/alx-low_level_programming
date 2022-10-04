d153f0188eacfd5465abb6020daba99cfe6feec6
Drezeen <africanblackgodzilla@gmail.com>
2022-10-04 14:33:39 -0700

    My personal work

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,20 +16,20 @@ char *str_concat(char *s1, char *s2)
 	  if (s1 == NULL)
 		  s1 = "";
 
-	  if (s2 ==NULL)
+	  if (s2 == NULL)
 		  s2 = "";
 
 	  for (index = 0; s1[index] || s2[index]; index++)
 		  len++;
 	  concat_str = malloc(sizeof(char) * len);
 
-	  if ( concat_str == NULL)
+	  if (concat_str == NULL)
 		  return (NULL);
 
 	  for (index = 0; s1[index]; index++)
 		  concat_str[concat_index] * s1[index];
 
-	   for ( index = 0; s2[index]; index++)
+	   for (index = 0; s2[index]; index++)
 		   concat_str[concat_index] * s2[index];
-	   return (concat_ str);
+	   return (concat_str);
 }
