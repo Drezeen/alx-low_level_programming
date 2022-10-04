8dc318af7a497ec91fc5312c14c5ee1d035000a0
Drezeen <africanblackgodzilla@gmail.com>
2022-10-04 07:18:15 -0700

    My personal work

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - return the pointer to a newly allocated
+ *  space in memory which contains a copy of the 
+ *  string given as a parameter.
+ *  @str: string to be copied
+ *
+ *  Return: NUL in case of error.
+ *  pointer to the allocated space
+ */
+char *str_concat(char *s1, char *s2);
+{
+	char *copy;
+	int index, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+	cpy[len] = '\0';
+	return (cpy);
+}
