ae72c31ccffbf4305b64b8b722b7771d6bfb93db
Drezeen <africanblackgodzilla@gmail.com>
2022-10-04 07:26:14 -0700

    My personal work

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,9 +9,8 @@
  *  Return: NUL in case of error.
  *  pointer to the allocated space
  */
-char *str_concat(char *s1, char *s2);
-{
-	char *copy;
+char *str_concat(char *s1, char *s2){
+	char *cpy;
 	int index, len;
 
 	if (str == NULL)
