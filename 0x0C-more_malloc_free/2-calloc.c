25e4335a4a003c72bb0dafeecdcb583995581eea
Drezeen <africanblackgodzilla@gmail.com>
2022-10-06 17:21:00 -0700

    My personal work

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,7 +6,7 @@
  * @nmemb: number of elements in array
  * @size: size in bytes of the elements
  *
- * return: void pointer to allocated memory
+ * Return: void pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
